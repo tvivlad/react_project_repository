35ad0133d4789386fbf3d19613393c626560c1ad
tvivlad <tvi-vlad@mail.ru>
2024-04-22 20:16:57 +0500

    user ingo page was created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
+  //  const email "george.bluth@reqres.in"
+  //  const first_name "George"
+  //  const last_name "Bluth"
+  //  const avatar "https://reqres.in/img/faces/1-image.jpg"
+  let showEmail = true
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='card'>
+      <div>
+        <img src='https://reqres.in/img/faces/1-image.jpg' alt='' />
+      </div>
+      <div>name: George</div>
+      <div>surname: Bluth</div>
+      <div>email: {showEmail && <span>george.bluth@reqres.in</span>}</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
