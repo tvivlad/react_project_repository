8eccfe0c6b0492a0b45bcda2326564682e597636
tvivlad <tvi-vlad@mail.ru>
2024-04-24 09:56:20 +0500

    changed font of the card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,9 +8,11 @@ const Card = ({ email, first_name, last_name, avatar }) => {
         <img src={avatar} alt='' />
       </div>
       <div>
-        <div>name: {first_name}</div>
-        <div>surname: {last_name}</div>
-        <div>email: {showEmail && <span>{email}</span>}</div>
+        <div style={{ fontSize: '28px' }}>name: {first_name}</div>
+        <div style={{ fontSize: '26px' }}>surname: {last_name}</div>
+        <div style={{ fontSize: '24px' }}>
+          email: {showEmail && <span>{email}</span>}
+        </div>
       </div>
     </div>
   )
