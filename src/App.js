229e965df060bc9ece3d290fa4b9f5d538420f07
tvivlad <tvi-vlad@mail.ru>
2024-05-11 17:39:38 +0500

    User card was created

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react'
+import Card from './components/Card'
 
 function App() {
-  //  const email "george.bluth@reqres.in"
-  //  const first_name "George"
-  //  const last_name "Bluth"
-  //  const avatar "https://reqres.in/img/faces/1-image.jpg"
-  let showEmail = true
+  const email = 'george.bluth@reqres.in'
+  const first_name = 'George'
+  const last_name = 'Bluth'
+  const avatar = 'https://reqres.in/img/faces/1-image.jpg'
+
   return (
-    <div className='card'>
-      <div>
-        <img src='https://reqres.in/img/faces/1-image.jpg' alt='' />
-      </div>
-      <div>name: George</div>
-      <div>surname: Bluth</div>
-      <div>email: {showEmail && <span>george.bluth@reqres.in</span>}</div>
+    <div>
+      <Card
+        name={first_name}
+        surname={last_name}
+        email={email}
+        avatar={avatar}
+      />
     </div>
   )
 }
