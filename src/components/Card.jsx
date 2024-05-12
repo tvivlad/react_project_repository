f9cb133a34d386cb6046809ee1c9d62d783a5bbe
mariiglchn <gali4inam170707@gmail.com>
2024-05-12 18:48:01 +0400

    User card was created

---
+++ src/components/Card.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Card = ({ name, surname, email, avatar }) => {
+  return (
+    <div className="card">
+      <div>
+        <img src={avatar} alt="" />
+      </div>
+      <div>name: {name}</div>
+      <div>surname: {surname}</div>
+      <div>
+        email: <span>{email}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
